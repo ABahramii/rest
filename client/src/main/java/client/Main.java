94bc2b9f737f4e0b64627d3ba172ae91fd881210
amir <a@gmail.com>
2022-01-21 21:43:44 +0330

    init commit

---
+++ client/src/main/java/client/Main.java
@@ -0,0 +1,7 @@
+package client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, client");
+    }
+}
---
+++ server/src/main/java/server/HelloApplication.java
@@ -0,0 +1,9 @@
+package server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}

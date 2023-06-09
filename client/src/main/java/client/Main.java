37f3e100c675ae027975bff012d412a5a226f973
amir <amirbahraami99@gmail.com>
2023-06-09 16:48:06 +0330

    remove client module

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,7 +1,23 @@
 package client;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello, client");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8080/server-1.0-SNAPSHOT/api/accounts/1"))
+                .setHeader("User-Agent", "first http request from JAVA 17")
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
